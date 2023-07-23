c3ed64fc615608ed73601d2e489fba359d494707
JackOfBlades232 <petrkiyashko232@gmail.com>
2023-07-23 10:22:46 +0300

    Completed AVL tree

--- algorithms/avl_tree.cpp
+++ algorithms/avl_tree.cpp
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX(_a, b) (_a > b ? _a : b)
+#define MAX(_a, _b) (_a > _b ? _a : _b)
+#define ABS(_a) (_a >= 0 ? _a : -_a)
 
 template <typename T>
 struct AVLTreeNode {
@@ -11,10 +12,11 @@ struct AVLTreeNode {
     int height;
 
     int disbalance();
-    void rotate_left();
-    void rotate_right();
-    void rotate_right_left();
-    void rotate_left_right();
+    void recalculate_height();
+    AVLTreeNode<T> *rotate_left();
+    AVLTreeNode<T> *rotate_right();
+    AVLTreeNode<T> *rotate_right_left();
+    AVLTreeNode<T> *rotate_left_right();
 };
 
 #define H(_node) (_node ? _node->height : 0)
@@ -26,63 +28,85 @@ int AVLTreeNode<T>::disbalance()
 }
 
 template <typename T>
-void AVLTreeNode<T>::rotate_left()
+void AVLTreeNode<T>::recalculate_height()
+{
+    height = MAX(H(right), H(left)) + 1;
+}
+
+template <typename T>
+AVLTreeNode<T> *AVLTreeNode<T>::rotate_left()
 {
     if (!right)
-        return; // @TODO: error
+        return nullptr; // @TODO: error
 
     right->parent = parent;
+    if (parent) {
+        if (this == parent->right)
+            parent->right = right;
+        else
+            parent->left = right;
+    }
     parent = right;
 
-    AVLTreeNode<T> tmp = right->left;
+    AVLTreeNode<T> *tmp = right->left;
     right->left = this;
 
     right = tmp;
     if (tmp)
         tmp->parent = this;
 
-    height = MAX(H(right), H(left));
-    parent->height = MAX(H(parent->right), H(parent->left));
+    recalculate_height();
+    parent->recalculate_height();
+
+    return parent;
 }
 
 template <typename T>
-void AVLTreeNode<T>::rotate_right()
+AVLTreeNode<T> *AVLTreeNode<T>::rotate_right()
 {
     if (!left)
-        return; // @TODO: error
+        return nullptr; // @TODO: error
 
     left->parent = parent;
+    if (parent) {
+        if (this == parent->right)
+            parent->right = left;
+        else
+            parent->left = left;
+    }
     parent = left;
 
-    AVLTreeNode<T> tmp = left->right;
+    AVLTreeNode<T> *tmp = left->right;
     left->right = this;
 
     left = tmp;
     if (tmp)
         tmp->parent = this;
 
-    height = MAX(H(right), H(left));
-    parent->height = MAX(H(parent->right), H(parent->left));
+    recalculate_height();
+    parent->recalculate_height();
+
+    return parent;
 }
 
 template <typename T>
-void AVLTreeNode<T>::rotate_right_left()
+AVLTreeNode<T> *AVLTreeNode<T>::rotate_right_left()
 {
     if (!right || !right->left)
-        return; // @TODO: error
+        return nullptr; // @TODO: error
 
     right->rotate_right();
-    rotate_left();
+    return rotate_left();
 }
 
 template <typename T>
-void AVLTreeNode<T>::rotate_left_right()
+AVLTreeNode<T> *AVLTreeNode<T>::rotate_left_right()
 {
     if (!left || !left->right)
-        return; // @TODO: error
+        return nullptr; // @TODO: error
 
     left->rotate_left();
-    rotate_right();
+    return rotate_right();
 }
 
 template <typename T>
@@ -91,6 +115,7 @@ private:
     AVLTreeNode<T> *m_root;
 
     AVLTreeNode<T> **m_search_ptr(T key, AVLTreeNode<T> **parent_out);
+    void m_remove_one_child(AVLTreeNode<T> **nodep);
     void m_rebalance(AVLTreeNode<T> *base);
     void m_print_subtree(AVLTreeNode<T> *root);
 
@@ -121,7 +146,38 @@ AVLTreeNode<T> **AVLTree<T>::m_search_ptr(T key, AVLTreeNode<T> **parent_out)
 template <typename T>
 void AVLTree<T>::m_rebalance(AVLTreeNode<T> *base)
 {
-    // @TODO: implement
+    if (!base)
+        return;
+
+    int prev_height = base->height;
+    AVLTreeNode<T> *grandparent = base->parent;
+
+    base->recalculate_height();
+    int disbalance = base->disbalance();
+    if (ABS(disbalance) > 1) {
+        AVLTreeNode<T> *new_base;
+
+        if (disbalance > 0) {
+            if (base->right->disbalance() < 0)
+                new_base = base->rotate_right_left();
+            else
+                new_base = base->rotate_left();
+        }
+        else {
+            if (base->left->disbalance() > 0)
+                new_base = base->rotate_left_right();
+            else
+                new_base = base->rotate_right();
+        }
+
+        if (new_base && !new_base->parent)
+            m_root = new_base;
+    } 
+
+    if (prev_height == base->height)
+        return;
+
+    m_rebalance(grandparent);
 }
 
 template <typename T>
@@ -131,8 +187,24 @@ void AVLTree<T>::m_print_subtree(AVLTreeNode<T> *root)
         return;
 
     m_print_subtree(root->left);
-    std::cout << root->key << '\n';
+    printf("%d, h%d, d%d\n", root->key, root->height, root->disbalance());
     m_print_subtree(root->right);
+}
+
+template <typename T>
+void AVLTree<T>::m_remove_one_child(AVLTreeNode<T> **nodep)
+{
+    AVLTreeNode<T> *node = *nodep;
+    AVLTreeNode<T> *next = node->right ? node->right : node->left;
+
+    if (next) {
+        if (node->parent)
+            next->parent = node->parent;
+        else
+            m_root = next;
+    }
+
+    *nodep = next;
 }
 
 template <typename T>
@@ -164,17 +236,34 @@ void AVLTree<T>::add(T key)
     new_node->height = 1;
     *nodep = new_node;
 
-    m_rebalance(new_node);
+    m_rebalance(parent);
 }
 
 template <typename T>
 void AVLTree<T>::remove(T key)
 {
-    AVLTreeNode<T> **nodep = m_search_ptr(key, nullptr);
+    AVLTreeNode<T> *parent;
+    AVLTreeNode<T> **nodep = m_search_ptr(key, &parent);
     if (!*nodep)
         return;
 
-    // @TODO: regular removal
+    if (!(*nodep)->right || !(*nodep)->left)
+        m_remove_one_child(nodep);
+    else {
+        AVLTreeNode<T> *node = *nodep;
+        AVLTreeNode<T> *repl = node->right;
+        while (repl->left)
+            repl = repl->left;
+
+        node->key = repl->key;
+        node = repl;
+        parent = node->parent;
+        nodep = node == parent->right ? &parent->right : &parent->left;
+
+        m_remove_one_child(nodep);
+    }
+
+    m_rebalance(parent);
 }
 
 template <typename T>
